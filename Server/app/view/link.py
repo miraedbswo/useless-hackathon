38d8ebc90c6bd5b04f5c958cf7509b96fa62e202
miraedbswo <miraedbswo@gmail.com>
2019-05-19 05:09:54 +0900

    시현을 위해 순서대로 값 전달

--- Server/app/view/link.py
+++ Server/app/view/link.py
@@ -12,17 +12,22 @@ from app.model.link import LinkModel
 
 
 def generator():
-    try:
+    while True:
         for i in range(0, 5):
-            yield i
-    except StopIteration:
-        return generator()
+            try:
+                yield i
+            except StopIteration:
+                pass
+
+
+gen = generator()
 
 
 class Link(Resource):
     @swag_from(LINK_GET)
     def get(self, phone_num: str):
-        favorite_food = generator()
+
+        favorite_food = next(gen)
         link: List[str, str] = LinkModel.objects(type=favorite_food).first().link
 
         return jsonify({
