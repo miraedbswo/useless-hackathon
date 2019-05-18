0af497de2ba70936863bac0555864b7faf33858f
miraedbswo <miraedbswo@gmail.com>
2019-05-18 22:42:56 +0900

    FIX: 오타 수정

--- Server/server.py
+++ Server/server.py
@@ -4,7 +4,7 @@ from app import create_app
 
 
 if __name__ == '__main__':
-    app = create_app('prodution')
+    app = create_app('production')
 
     if 'SECRET_KEY' not in os.environ:
         print('[WARN] SECRET KEY is not set in the environment variable !!')
